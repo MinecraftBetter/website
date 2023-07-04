ee01bc19c6dbada3c48b12614e5594387611a6ed
EvanG <evan_g@orange.fr>
2023-07-05 00:44:34 +0200

    Block web browsers from accessing Jellyfin

---
+++ jellyfin/web-browser.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Jellyfin Better</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            .jumbotron { margin-top: 50px; }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+					<small>Erreur 403</small>
+               <h1>Nous avons detecté un navigateur 😑</h1>
+               <p>Comme spécifié dans la description du service (que vous avez bien entendu lu, n'est-ce pas), les navigateurs ne sont pas autorisés sur ce service pour des raisons d'utilisation des ressources serveur.</p>
+					<p>Merci de télécharger un client Jellyfin, une liste non-exaustive vous est proposée <a href="https://justbetter.fr/details#jellyfinbetter">ici</a></p>
+					<p>Si vous pensez que ce message est une erreur, merci de contacter <b><em>evang_</em></b> sur Discord</p>
+            </div>
+        </div>
+		  <script>
+				window.onload = () => {
+					let searchParams = new URLSearchParams(window.location.search);
+					let url = new URL(searchParams.get("url"));
+					url.searchParams.set("bypass", "true");
+					colorTrace("If you know what you're doing you can bypass this check", "lightblue", 20);
+					colorTrace(url.href, "lightblue", 15);
+					colorTrace("Please note that it will only bypass for this specific request and that the plateform still won't be usable.", "red", 15);
+				};
+				function colorTrace(msg, color, size) {
+					console.log("%c" + msg, "color:" + color + ";font-weight:bold;font-size:" + size + "pt");
+				}
+		  </script>
+    </body>
+</html>
