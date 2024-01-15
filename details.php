cb768d7455be0e78d472e3fa8aacaa808e7369cf
Evan Galli <evan_g@orange.fr>
2024-01-15 20:06:13 +0100

    docs: Update Gameyfin & Retrofin description

--- details.php
+++ details.php
@@ -67,7 +67,9 @@
                     S'il a un problème avec un des films/séries/animés, merci de le signaler sur cette <a href="https://request.justbetter.fr">même plateforme</a>.
                 </p>
                 <div role="alert" class="alert alert-dark">
-                    <i class="fas fa-info-circle" aria-hidden="true"></i> Il est nécessaire de disposer d'un compte pour pouvoir utiliser ce service, pour en créer un, utilisez <a href="https://jfago.justbetter.fr/invite/tM7jmbFsFGL2tLuYdRFyTT">l'assistant</a>.</div>
+                    <i class="fas fa-info-circle" aria-hidden="true"></i> Il est nécessaire de disposer d'un compte pour pouvoir utiliser ce service, pour en créer un, utilisez <a
+                        href="https://jfago.justbetter.fr/invite/tM7jmbFsFGL2tLuYdRFyTT">l'assistant</a>.
+                </div>
                 <p>
                     Toute personne créant un compte est tenue de rejoindre <a href="https://discord.gg/Dce4dewQ7D">notre Discord</a> et <strong>d'y rester</strong> sous peine d'un
                     bannissement.<br/>
@@ -75,26 +77,34 @@
                     Cette mesure nous permet de pouvoir communiquer avec vous si des changements importants ont lieu.
                 </p>
                 <p>
-                    <div role="alert" class="alert alert-danger">
-                        <i class="fas fa-exclamation-circle" aria-hidden="true"></i>
-                        Il vous est demandé d'<strong>envoyer un message</strong> dans le salon <code>#général</code> du serveur Discord en mentionnant qui vous a communiqué le lien du serveur.
-                        À défaut de l'avoir fait, vous pourriez faire l'objet d'un bannissement temporaire.
-                    </div>
+                <div role="alert" class="alert alert-danger">
+                    <i class="fas fa-exclamation-circle" aria-hidden="true"></i>
+                    Il vous est demandé d'<strong>envoyer un message</strong> dans le salon <code>#général</code> du serveur Discord en mentionnant qui vous a communiqué le lien du
+                    serveur.
+                    À défaut de l'avoir fait, vous pourriez faire l'objet d'un bannissement temporaire.
+                </div>
                 </p>
                 <p>
-                    Pour utiliser ce service, il vous est <strong>requis d'installer un client Jellyfin</strong>, cela permet de grandement réduire la charge du serveur et ainsi pouvoir accueillir plus de monde.<br/>
+                    Pour utiliser ce service, il vous est <strong>requis d'installer un client Jellyfin</strong>, cela permet de grandement réduire la charge du serveur et ainsi
+                    pouvoir accueillir plus de monde.<br/>
                     Voici une liste non exhaustive des clients disponibles:
-                    <ul>
-                        <li><strong>Windows:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.9.1/JellyfinMediaPlayer-1.9.1-windows-x64.exe">Jellyfin Media Player</a></li>
-                        <li><strong>MacOS:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.9.1/JellyfinMediaPlayer-1.9.1-macos-notarized.dmg">Jellyfin Media Player</a></li>
-                        <li><strong>Linux:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/">Jellyfin Media Player</a></li>
-                        <li><strong>Android:</strong> <a href="https://play.google.com/store/apps/details?id=dev.jdtech.jellyfin">Findroid</a> <small class="text-danger">(Ne pas utiliser l'application Jellyfin !)</small></li>
-                        <li><strong>iOS:</strong> <a href="https://apps.apple.com/ca/app/swiftfin/id1604098728">Swiftfin</a> <small class="text-danger">(Ne pas utiliser l'application Jellyfin Mobile !)</small></li>
-                        <li><strong>Android TV:</strong> <a href="https://play.google.com/store/apps/details?id=org.jellyfin.androidtv">Jellyfin for Android TV</a></li>
-                        <li><strong>Tizen (Samsung):</strong> <a href="https://github.com/jeppevinkel/jellyfin-tizen-builds">Jellyfin for Tizen</a> <small class="text-warning">(Programme en alpha à installer manuellement, s'adresser à <strong><em>evang_</em></strong> pour toute demande d'aide)</small></li>
-                        <li><strong>WebOS (LG):</strong> <a href="https://us.lgappstv.com/main/tvapp/detail?appId=1030579">Jellyfin for WebOS</a> <small class="text-danger">(Non fonctionnel sur le serveur ! Si vous souhaitez nous aider à régler ce problème, merci de contacter <strong><em>evang_</em></strong>)</small></li>
-                    </ul>
-                    Sur le client, veuillez entrer <code>https://jellyfin.justbetter.fr</code> en tant qu'adresse du serveur.
+                <ul>
+                    <li><strong>Windows:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.9.1/JellyfinMediaPlayer-1.9.1-windows-x64.exe">Jellyfin
+                            Media Player</a></li>
+                    <li><strong>MacOS:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.9.1/JellyfinMediaPlayer-1.9.1-macos-notarized.dmg">Jellyfin
+                            Media Player</a></li>
+                    <li><strong>Linux:</strong> <a href="https://github.com/jellyfin/jellyfin-media-player/releases/">Jellyfin Media Player</a></li>
+                    <li><strong>Android:</strong> <a href="https://play.google.com/store/apps/details?id=dev.jdtech.jellyfin">Findroid</a> <small class="text-danger">(Ne pas
+                            utiliser l'application Jellyfin !)</small></li>
+                    <li><strong>iOS:</strong> <a href="https://apps.apple.com/ca/app/swiftfin/id1604098728">Swiftfin</a> <small class="text-danger">(Ne pas utiliser l'application
+                            Jellyfin Mobile !)</small></li>
+                    <li><strong>Android TV:</strong> <a href="https://play.google.com/store/apps/details?id=org.jellyfin.androidtv">Jellyfin for Android TV</a></li>
+                    <li><strong>Tizen (Samsung):</strong> <a href="https://github.com/jeppevinkel/jellyfin-tizen-builds">Jellyfin for Tizen</a> <small class="text-warning">(Programme
+                            en alpha à installer manuellement, s'adresser à <strong><em>evang_</em></strong> pour toute demande d'aide)</small></li>
+                    <li><strong>WebOS (LG):</strong> <a href="https://us.lgappstv.com/main/tvapp/detail?appId=1030579">Jellyfin for WebOS</a> <small class="text-danger">(Non
+                            fonctionnel sur le serveur ! Si vous souhaitez nous aider à régler ce problème, merci de contacter <strong><em>evang_</em></strong>)</small></li>
+                </ul>
+                Sur le client, veuillez entrer <code>https://jellyfin.justbetter.fr</code> en tant qu'adresse du serveur.
                 </p>
                 <p class="lead" id="buttons" style="flex-wrap: wrap; text-align: center;">
                     <a href="https://jellyfin.justbetter.fr/" class="btn btn-custom-primary btn-lg">
@@ -127,9 +137,10 @@
                         <p class="mb-0">Des jeux en toute légalité et sans DRM</p>
                     </div>
                 </div>
-                <p><strong>Gameyfin</strong> est une plateforme ne contenant que des jeux <strong>sans DRM</strong> obtenus de manière <strong>légale</strong>.</p>
+                <p><strong>Gameyfin</strong> est une plateforme ne contenant que des jeux PC <strong>sans DRM</strong> obtenus de manière <strong>légale</strong>.</p>
                 <p>Si vous souhaitez participer à l'étoffement du catalogue, merci de contacter <strong><em>mikaleplubo</em></strong>.</p>
                 <p>Dû à la quantité de jeux mis à votre disposition, tous n'ont pu être testé, si un jeu ne se lance pas, merci de nous en informer.</p>
+                <p>Sur Windows, le client <a href="https://gamevau.lt/">GameVault</a> est supporté par notre plateforme, connectez-vous au serveur avec l'URL <code>https://clientgameyfin.justbetter.fr/</code></p>
 
                 <p class="lead" id="buttons" style="flex-wrap: wrap; text-align: center;">
                     <a href="https://gameyfin.justbetter.fr/" class="btn btn-custom-primary btn-lg">
@@ -146,6 +157,12 @@
                     </div>
                 </div>
                 <p>Sur <strong>Retrofin</strong>, vous pourrez trouver les ROMs d'un grand nombre de jeux rétro (ou console de manière plus générale).</p>
+                <p>Vous trouverez aussi pour certains jeux, des packs HD permettant de jouer à vos jeux favoris tout en profitant d'une qualité améliorée</p>
+                <p>Vous pouvez télécharger les ROMs et Pack HD pour y jouer localement ou jouer directement dans votre navigateur si la console est supportée <em>(bientôt disponible)</em></p>
+                <div role="alert" class="alert alert-dark">
+                    <i class="fas fa-info-circle" aria-hidden="true"></i>
+                    Pour vous connecter à la plateforme, utilisez<br/>Identifiant : <code>guest</code><br/>Mot de passe : <code>guest</code>
+                </div>
 
                 <p class="lead" id="buttons" style="flex-wrap: wrap; text-align: center;">
                     <a href="https://retrofin.justbetter.fr/" class="btn btn-custom-primary btn-lg">
@@ -161,7 +178,8 @@
                         <p class="mb-0">Toile de dessin collaborative</p>
                     </div>
                 </div>
-                <p>Sur <strong>Better Place</strong>, vous pourrez dessiner sur une grande toile collaborative à la manière de <a href="https://www.reddit.com/r/place">r/place</a>.</p>
+                <p>Sur <strong>Better Place</strong>, vous pourrez dessiner sur une grande toile collaborative à la manière de <a href="https://www.reddit.com/r/place">r/place</a>.
+                </p>
                 <p>Seul ou à plusieurs, contribuez à remplir la toile avec de nombreux dessins.</p>
                 <p class="mt-3 text-warning">Toute tentative de vandalisme est strictement interdite et conduira inéluctablement au bannissement du mis en cause.</p>
 
@@ -179,7 +197,8 @@
             <p>Ce site a été codé avec ❤ par <a href="https://github.com/06Games" style="font-weight: 600;">Evan Galli</a></p>
             <p>Ce serveur est généreusement hébergé par <a href="https://github.com/Tiagoez" style="font-weight: 600;">Tiago Procopio</a></p>
             <p>Le design a été imaginé par <a href="https://github.com/MazeWave" style="font-weight: 600;">Louis Dalmasso</a></p>
-            <p>Merci à <a href="https://github.com/Mikaleplubo" style="font-weight: 600;">Mika </a> <small style="font-size: 7px;">(le 🐐)</small> pour son investissement quotidien dans le serveur</p>
+            <p>Merci à <a href="https://github.com/Mikaleplubo" style="font-weight: 600;">Mika </a> <small style="font-size: 7px;">(le 🐐)</small> pour son investissement quotidien
+                dans le serveur</p>
         </div>
     </footer>
 </div>
