fb6e54fb73a48d9c81dc4db55321b095c6776b47
Evan Galli <evan_g@orange.fr>
2024-11-09 18:25:42 +0100

    feat(Gameyfin): Improved mobile display of the landing page

--- gameyfin/index.php
+++ gameyfin/index.php
@@ -20,16 +20,16 @@
 </head>
 
 <body class="d-flex align-items-center">
-<div class="mx-auto">
+<div class="mx-auto" style="max-width: calc(100vw - 3em);max-height: calc(100vh - 3em);">
     <main role="main">
         <div class="d-flex flex-column w-100 introduction overflow-hidden" style="min-height: unset;">
             <div class="text-center" style="position: relative;">
                 <img src="/assets/img/logos/gameyfin.png" href="#" alt="Gameyfin logo" class="presentation-icon"/>
                 <h1 style="margin-bottom: 2.5rem;">Gameyfin</h1>
-                <p class="lead">La plateforme à laquelle vous accédez ne contient que des jeux <strong>sans DRM</strong> obtenus de manière <strong>légale</strong>.</p>
-                <p class="lead">Si vous souhaitez participer à l'étoffement du catalogue, merci de contacter <strong><em>mikaleplubo</em></strong>.</p>
-                <p class="lead">Dû à la quantité de jeux mis à votre disposition, tous n'ont pu être testé,<br/>si un jeu ne se lance pas, merci de nous en informer.</p>
-                <p class="lead" id="buttons" style="flex-wrap: wrap; margin-top: 2.5rem; justify-content: center;">
+                <p class="lead text-wrap">La plateforme à laquelle vous accédez ne contient que des jeux <strong>sans DRM</strong> obtenus de manière <strong>légale</strong>.</p>
+                <p class="lead text-wrap">Si vous souhaitez participer à l'étoffement du catalogue, merci de contacter <strong><em>mikaleplubo</em></strong>.</p>
+                <p class="lead text-wrap">Dû à la quantité de jeux mis à votre disposition, tous n'ont pu être testé,<br/>si un jeu ne se lance pas, merci de nous en informer.</p>
+                <p class="lead text-wrap" id="buttons" style="flex-wrap: wrap; margin-top: 2.5rem; justify-content: center;">
                     <a href="#" data-link="library" class="btn btn-lg btn-dark" style="max-width: 500px;" id="goToService">Accéder au catalogue</a>
                 </p>
             </div>
